917d7b6167aa483ff6468d537db178b7a5255af1
vinceluv88 <vinceluv@gmail.com>
2026-09-09 07:27:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=864d72fb-aa3a-4479-883a-a49c97cd1005",
     # 添加更多链接
 ]
 
